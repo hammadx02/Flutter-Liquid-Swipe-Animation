370113a4257c75c3351c114c5cde2e05b74048e3
Hammad Ali <hammadaali02@gmail.com>
2024-09-15 19:22:24 +0500

    updated

--- lib/1.dart
+++ lib/1.dart
@@ -16,8 +16,7 @@ class _HomePageState extends State<HomePage> {
     SizedBox(
       height: double.infinity,
       child: CachedNetworkImage(
-        imageUrl:
-            'https://plus.unsplash.com/premium_photo-1675200124904-dfadce24119f?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        imageUrl: 'https://pin.it/1WvkLMJu3',
         fit: BoxFit.cover,
         placeholder: (context, url) =>
             const Center(child: CircularProgressIndicator()),
