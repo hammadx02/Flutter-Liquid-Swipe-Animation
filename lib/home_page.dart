61781e01b114e3254bd0e87c62828fad412064e4
Hammad Ali <hammadaali02@gmail.com>
2024-09-16 23:10:08 +0500

    updated

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,4 @@
-ignore_for_file: library_private_types_in_public_api
+// ignore_for_file: library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
